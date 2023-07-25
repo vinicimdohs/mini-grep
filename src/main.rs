138d60e49b05a488a76579313b6682f06a4b39cb
vinicimdohs <vinicim.fernandez1@gmail.com>
2023-07-25 15:34:09 -0300

    feat: implement test with tdd and search function

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,6 @@ fn main() {
         process::exit(1);
     });
 
-    println!("serching for {}",config.query);
-    println!("in File {}",config.file_path);
-
     if let Err(e) =  minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
